bfcfb23bf8b2b94b3cec63a6bb29658f9460bdbb
ststeiger <quandary82@hailmail.net>
2019-09-13 17:37:17 +0200

    WebTerm Update to .NET Core 2.1

---
+++ WebTerm/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebTerm.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
